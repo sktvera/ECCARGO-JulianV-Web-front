051492aa1b85c928816654b4f3a20d34b1731adc
julian vera <julian.vera@enmediolabs.com>
2024-04-18 01:03:52 -0500

    depurando front

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,17 @@ import Mens from'./Routes/Mens/Mens'
 import Woman from './Routes/Woman/Woman';
 import Footer from "./Components/Includes/Footer/Footer";
 import Header from "./Components/Includes/Header/Header";
-import Stores from './Components/stores/stores'
-import Things from './Routes/Things/Things'
+import Things from "./Routes/Things/Things";
+import Stores from "./Components/stores/stores";
 
-import Task from './Routes/Task/Task';
+
+
 
 import './App.css'
 
-//Redux
-import { Provider } from 'react-redux';
-import {store} from './store/store';
 
 function App() {
-  return (<Provider store={store}>
+  return (
     <div className="App">
       <Header/>
     <Routes>
@@ -25,15 +23,14 @@ function App() {
       <Route path='/ECCARGO' element={<Welcome/>}/>
       <Route path="/Mens" element={<Mens/>}/>
       <Route path="/Quotation" element={<Woman/>}/>
-      {/* <Route path="/task/:id" element={<Task/>}/> */}
-     {/*  <Route path="/store" element={<Stores/>}/>
-      <Route path="/things" element={<Things/>}/> */}
+      <Route path="/store" element={<Stores/>}/>
+      <Route path="/things" element={<Things/>}/>
      
     </Routes>
     <Footer/>
     </div>
     
-  </Provider>
+ 
   );
 }
 
